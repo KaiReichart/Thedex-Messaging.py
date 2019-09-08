49c34c46074324d5e154d381ccc2f101b0b26910
Kai Reichart <kai@reichart-webdesign.de>
2019-09-08 16:28:20 +0200

    pypi

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This package generates Messages for the THEDEX API.
 API Secifications can be found [at the official Sovdwaer Page](https://sovdwaer.de/files/content/dokumente/THEDEX_Entwicklerinformation.pdf).
 
+## Build and publish
+1. Create Tag & Release in Github
+2. Update Download-URL in `setup.py`
+3. Build and upload:
+
+    ```bash
+    $ python setup.py sdist
+    $ twine upload dist/* 
+    ```
--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Thedex_Messaging",
-    version="0.0.1",
+    version="0.0.2",
     author="Kai Reichart",
     author_email="kai@reichart.dev",
     description="A Package to create Messages for the Thedex_Messaging API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KaiReichart/Thedex-Messaging.py",
-    download_url="https://github.com/KaiReichart/Thedex-Messaging.py/archive/v0.0.1.tar.gz",
+    download_url="https://github.com/KaiReichart/Thedex-Messaging.py/archive/v0.0.2.tar.gz",
     packages=setuptools.find_packages(),
     install_requires=[            # I get to this in a second
           'datetime',
       ],
     classifiers=[
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
